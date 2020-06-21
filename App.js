9a4ce4d56609636300451dd85fbc1d7eb944c280
Tirthankar Kundu <tirthankar.kundu94@gmail.com>
2020-06-22 01:23:17 +0530

    Flex basic

--- App.js
+++ App.js
@@ -16,61 +16,31 @@ import {
 } from "react-native";
 
 export default function App() {
-  // console.log(require("./assets/icon.png"));
-
-  const handlePress = () => {
-    console.log("Clicked");
-  };
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-        Velit commodo eu do
-      </Text>
-
-      <TouchableOpacity onPress={() => console.log("CK")}>
-        <Image
-          // blurRadius={5}
-          fadeDuration={1000} //works only on android
-          style={styles.logo}
-          source={{ uri: "https://picsum.photos/200/300" }}
-        />
-      </TouchableOpacity>
-
-      <Button
-        color="orange"
-        title="ClickMe"
-        onPress={() =>
-          Alert.alert("Title", "Tapped", [
-            {
-              text: "Yes",
-              onPress: () => {
-                console.log("yes");
-              },
-            },
-            {
-              text: "No",
-              onPress: () => {
-                console.log("no");
-              },
-            },
-          ])
-        }
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "whitesmoke",
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "dodgerblue",
+        }}
       />
-    </SafeAreaView>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "gold",
+        }}
+      />
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "tomato",
+        }}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
-  logo: {
-    width: "100%",
-    height: "50%",
-  },
-});
