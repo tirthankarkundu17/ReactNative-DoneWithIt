f6a14b68c258f5e6f8cb450e14d8d03a25bed473
Tirthankar Kundu <tirthankar.kundu94@gmail.com>
2020-10-02 23:08:41 +0530

    added styling changes

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  Image,
-  TouchableWithoutFeedback,
-  TouchableHighlight,
-  TouchableOpacity,
-  TouchableNativeFeedback, //Only works for Android
-  SafeAreaView,
-  Alert,
-  Platform,
-  StatusBar,
-} from "react-native";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import AppText from "./app/components/AppText";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import { View } from "react-native";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View>
+      <MaterialCommunityIcons name="email" size={200} color="dodgerblue" />
+      <AppText>How are you ?</AppText>
+    </View>
+  );
 }
